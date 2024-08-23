7af6a8b382f5d705b7c9dc69f1623862bab68b2f
thaylla <thayllachristineo@gmail.com>
2024-08-23 09:54:17 -0300

    feat: add sizes to Logo component

--- src/components/Logo/Logo.test.tsx
+++ src/components/Logo/Logo.test.tsx
@@ -1,17 +1,23 @@
 import { render, screen } from '@testing-library/react';
-
 import Logo from './Logo';
 
-describe('<Logo />', () => {
+describe('Logo component', () => {
   it('renders the correct image based on the variant', () => {
     render(<Logo variant="logomark" />);
     const image = screen.getByAltText('Logo do E-Healthy');
     expect(image).toHaveAttribute('src', 'logomark.svg');
   });
 
-  it('renders the core image if no variant is provided', () => {
-    render(<Logo />);
+  it('renders the correct size based on the size prop', () => {
+    render(<Logo size="large" />);
     const image = screen.getByAltText('Logo do E-Healthy');
-    expect(image).toHaveAttribute('src', 'core.svg');
+    expect(image).toHaveAttribute('width', '360');
+    expect(image).toHaveAttribute('height', '360');
+  });
+
+  it('renders the correct height for the wordmark variant', () => {
+    render(<Logo variant="wordmark" size="medium" />);
+    const image = screen.getByAltText('Logo do E-Healthy');
+    expect(image).toHaveAttribute('height', '120');
   });
 });
