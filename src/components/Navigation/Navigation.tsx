ce8c99454adbbc230d8d4b70c83b378f23ff6e73
thaylla <thayllachristineo@gmail.com>
2024-08-26 19:25:16 -0300

    chore: applies classNames into navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,16 +1,23 @@
 'use client';
 
-import './Navigation.scss';
+import { FC } from 'react';
+import classNames from 'classnames';
 
 import Logo from '@/components/Logo';
 import useMediaQuery from '@/hooks/useMediaQuery';
 
-const Navigation: React.FC = () => {
+import styles from './Navigation.module.scss';
+
+type TProps = {
+  className?: string;
+};
+
+const Navigation: FC<TProps> = ({ className }) => {
   const isMobile = useMediaQuery('(max-width: 768px)');
   const logoSize = isMobile ? 'xxsmall' : 'xsmall';
 
   return (
-    <nav>
+    <nav className={classNames(styles['c-navigation'], className)}>
       <div>
         <Logo variant="wordmark" size={logoSize} />
       </div>
