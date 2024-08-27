f7c9fa898916a09162d843d2fe63b9db948014f3
thaylla <thayllachristineo@gmail.com>
2024-08-27 13:04:01 -0300

    chore: rewrite tests to have a pattern

--- src/components/Navigation/Navigation.test.tsx
+++ src/components/Navigation/Navigation.test.tsx
@@ -6,21 +6,21 @@ import Navigation from './Navigation';
 const setup = () => render(<Navigation />);
 
 describe('<Navigation />', () => {
-  it('renders the Logo component with the correct variant and size', () => {
+  it('should render the Logo component with the correct variant and size', () => {
     createMatchMedia(1024);
     setup();
     const logo = screen.getByAltText('Logo do E-Healthy');
     expect(logo).toHaveAttribute('src', 'wordmark.svg');
-    expect(logo).toHaveAttribute('width', '144');
     expect(logo).toHaveAttribute('height', '48');
+    expect(logo).toHaveAttribute('width', '144');
   });
 
-  it('renders the Logo component with the correct size for smaller viewports', () => {
+  it('should render the Logo component with the correct size for smaller viewports', () => {
     createMatchMedia(768);
     setup();
     const logo = screen.getByAltText('Logo do E-Healthy');
     expect(logo).toHaveAttribute('src', 'wordmark.svg');
-    expect(logo).toHaveAttribute('width', '104');
     expect(logo).toHaveAttribute('height', `${104 / 3}`);
+    expect(logo).toHaveAttribute('width', '104');
   });
 });
