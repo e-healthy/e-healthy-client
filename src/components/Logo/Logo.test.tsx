60f22f2c9194ba4199bf2a596419ebdf064350fa
thaylla <thayllachristineo@gmail.com>
2024-11-12 13:54:56 -0300

    feat: add prop color when logo is white

--- src/components/Logo/Logo.test.tsx
+++ src/components/Logo/Logo.test.tsx
@@ -1,33 +1,35 @@
 import { render, screen } from '@testing-library/react';
 
-import Logo from './Logo';
+import Logo, { TProps } from './Logo';
 
-const defaultProps = {
+const defaultProps: TProps = {
   variant: 'core',
   size: 'medium',
+  color: 'default',
 };
 
 const setup = (props = defaultProps) => render(<Logo {...props} />);
 
 describe('<Logo />', () => {
+  it('should render the default logo', () => {
+    render(<Logo />);
+    const image = screen.getByAltText('Logo da E-Healthy');
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', '/core.svg');
+    expect(image).toHaveAttribute('height', '144');
+    expect(image).toHaveAttribute('width', '144');
+  });
+
   it('should render the correct image based on the variant', () => {
     setup({ ...defaultProps, variant: 'logomark' });
     const image = screen.getByAltText('Logo da E-Healthy');
     expect(image).toHaveAttribute('src', '/logomark.svg');
   });
 
-  it('should render the correct larger height and width', () => {
-    setup({ ...defaultProps, size: 'large' });
+  it('should render the correct white variant when color is passed', () => {
+    setup({ ...defaultProps, variant: 'logomark', color: 'white' });
     const image = screen.getByAltText('Logo da E-Healthy');
-    expect(image).toHaveAttribute('height', '160');
-    expect(image).toHaveAttribute('width', '160');
-  });
-
-  it('should render the correct medium height and width', () => {
-    setup({ ...defaultProps, size: 'medium' });
-    const image = screen.getByAltText('Logo da E-Healthy');
-    expect(image).toHaveAttribute('height', '144');
-    expect(image).toHaveAttribute('width', '144');
+    expect(image).toHaveAttribute('src', '/logomark-white.svg');
   });
 
   it('should render the correct smaller height and width', () => {
@@ -35,5 +37,19 @@ describe('<Logo />', () => {
     const image = screen.getByAltText('Logo da E-Healthy');
     expect(image).toHaveAttribute('height', '128');
     expect(image).toHaveAttribute('width', '128');
+  });
+
+  it('should render the correct medium height and width', () => {
+    setup();
+    const image = screen.getByAltText('Logo da E-Healthy');
+    expect(image).toHaveAttribute('height', '144');
+    expect(image).toHaveAttribute('width', '144');
+  });
+
+  it('should render the correct larger height and width', () => {
+    setup({ ...defaultProps, size: 'large' });
+    const image = screen.getByAltText('Logo da E-Healthy');
+    expect(image).toHaveAttribute('height', '160');
+    expect(image).toHaveAttribute('width', '160');
   });
 });
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,33 +2,45 @@ import { FC } from 'react';
 import Image from 'next/image';
 import classNames from 'classnames';
 
-const variants: Record<string, string> = {
-  core: '/core.svg',
-  logomark: '/logomark.svg',
-  wordmark: '/wordmark.svg',
-};
-
-const sizes: Record<string, number> = {
+const SIZES = {
   small: 128,
   medium: 144,
   large: 160,
+} as const;
+
+type Color = 'default' | 'white';
+type Variant = 'core' | 'logomark' | 'wordmark';
+type Size = keyof typeof SIZES;
+
+export type TProps = {
+  variant?: Variant;
+  size?: Size;
+  color?: Color;
 };
 
-type TProps = {
-  variant?: string;
-  size?: string;
+const getVariantPath = (variant: Variant, color: Color) => {
+  const colorSuffix = color === 'white' ? '-white' : '';
+  return `/${variant}${colorSuffix}.svg`;
 };
 
-const Logo: FC<TProps> = ({ variant = 'core', size = 'medium' }) => {
-  const wordmarkHeight = variant === 'wordmark' ? sizes[size] / 4 : sizes[size];
+const Logo: FC<TProps> = ({
+  variant = 'core',
+  size = 'medium',
+  color = 'default',
+}) => {
+  const wordmarkHeight = variant.startsWith('wordmark')
+    ? SIZES[size] / 4
+    : SIZES[size];
 
+  const src = getVariantPath(variant, color);
+  console.log(src);
   return (
     <Image
       className={classNames('c-logo')}
-      src={variants[variant]}
+      src={src}
       alt="Logo da E-Healthy"
-      width={sizes[size]}
       height={wordmarkHeight}
+      width={SIZES[size] ?? SIZES['medium']}
       priority
     />
   );
